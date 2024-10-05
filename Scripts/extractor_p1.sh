45e1256c7d69e0d6c934f70eb7cdb7f35f95b8c8
miguelrosasvillalobos <miguel.rosas2001@alumnos.ubiobio.cl>
2024-10-05 18:18:59 -0300

    se corrige tamano de ciclo for

--- Scripts/extractor_p1.sh
+++ Scripts/extractor_p1.sh
@@ -1,6 +1,6 @@
 # Lista de valores para v
 valores_lc=("lc1" "lc2" "lc3" "lc4")
-for j in {0..7}; do
+for j in {0..3}; do
   # Crear un archivo temporal para el script modificado
   temp_script="temp_script_${valores_lc[$j]}.py"
 
